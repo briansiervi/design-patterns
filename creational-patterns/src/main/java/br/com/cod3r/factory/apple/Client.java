2bb50e652f56db0959f5464986cba33c87e3b517
briansiervi <brianmazini@gmail.com>
2025-06-02 22:54:59 -0300

    feat(01-creational-patterns): factory - criando uma fábrica para cada objeto concreto

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/Client.java
@@ -1,11 +1,21 @@
 package br.com.cod3r.factory.apple;
 
+import br.com.cod3r.factory.apple.factory.IPhone11ProFactory;
+import br.com.cod3r.factory.apple.factory.IPhoneFactory;
+import br.com.cod3r.factory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public class Client {
-	
 	public static void main(String[] args) {
-		
+		IPhoneFactory iphoneXFactory = new IPhoneXFactory();
+		IPhoneFactory iphone11ProFactory = new IPhone11ProFactory();
+
 		System.out.println("### Ordering an iPhone X");
-		
-		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		IPhone iphone = iphoneXFactory.orderIPhone();
+		System.out.println(iphone);
+
+		System.out.println("\n\n### Ordering an iPhone 11 Pro");
+		IPhone iphone2 = iphone11ProFactory.orderIPhone();
+		System.out.println(iphone2);
 	}
 }
--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,6 +1,17 @@
 package br.com.cod3r.factory.apple.factory;
 
-public abstract class IPhoneFactory {
+import br.com.cod3r.factory.apple.model.IPhone;
 
-	
+public abstract class IPhoneFactory {
+  public IPhone orderIPhone() {
+    IPhone device = null;
+    device = createIPhone();
+    device.getHardware();
+    device.assemble();
+    device.certificates();
+    device.pack();
+    return device;
+  }
+
+  protected abstract IPhone createIPhone();
 }
